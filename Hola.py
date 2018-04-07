0e6476f19d7fec938c9a7d0d354b6a1c7ad28a54
Ana Chávez <38160675+anachz19@users.noreply.github.com>
2018-04-07 20:35:38 +0200

    Codigos dia 1 (7/04/18)
    codigos chidoris

---
+++ Hola.py
@@ -0,0 +1,28 @@
+"""
+print("Hola, ¿Como te llamas?")
+Nombre= "Ana"
+Apellido= "Chavez"
+Edad=24
+Genero="Femenino"
+Comidafavorita= "Pizza"
+Signozodiacal="Escorpion"
+Colordeojos= "cafe"
+print(Nombre,"\n", Apellido,"\n", Edad,"\n", Genero,"\n", Comidafavorita,"\n", Signozodiacal,"\n", Colordeojos,"\n")   
+"""
+#Codigo2
+Nombre= str(input("Dame tu nombre: "))
+Apellido= str (input("Dame tu apellido: "))
+Edad=int (input("Dime tu edad: "))
+Genero=str (input("Genero: "))
+Comidafavorita=str  (input("¿Cual es tu comida favorita? "))
+Signozodiacal=str (input("Sgino zodiacal: "))
+Colordeojos= str (input("Color de ojos: "))
+ 
+print("Nombre: ", Nombre,"\n",
+      "Apellido: ", Apellido,"\n",
+      "Edad: ",Edad,"\n",
+      "Genero: ", Genero,"\n",
+      "Comidafavorita: ", Comidafavorita,"\n",
+      "Signozodiacal: ", Signozodiacal,"\n",
+      "Colordeojos: ", Colordeojos,"\n")
+
