eca966b61cb3b87d8bbf0645d895779e0f226fe1
Ana Chávez <38160675+anachz19@users.noreply.github.com>
2018-04-14 10:47:26 -0500

    Add files via upload

---
+++ IVA.py
@@ -0,0 +1,8 @@
+print("Este programa calcula el IVA")
+Cantidad= float(input("Dame una cantidad"))
+IVA= float( input("Dame el IVA"))
+Resultado= Cantidad*IVA;
+print("Tu cantidad es: ", Cantidad, "y tu IVA es ", IVA,
+        "Tu cantidad total es: ", Resultado)
+
+input(".....")
---
+++ numeros.py
@@ -0,0 +1,7 @@
+print("Suma de numeros")
+Num1= int( input("Dame un numero: "))
+Num2= int( input("Dame otro numero: "))
+resultado= Num1+Num2
+print("El resultado de la suma es: ", resultado)
+
+input("Presione una tecla para continuar")
