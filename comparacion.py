3d1175c0a73afecb097a5e45f702f269b9fb2b15
Ana Chávez <38160675+anachz19@users.noreply.github.com>
2018-04-21 11:23:49 -0500

    Update comparacion.py

--- comparacion.py
+++ comparacion.py
@@ -1,10 +1,11 @@
 numero= int(input("Ingrese un numero"))
 b= int(input("Ingrese otro numero"))
+
+if numero<b:
+    print("El numero a es menor que b y son diferentes")
+else:
+    print("El numero a es mayor que b y son diferentes")
 if numero==b:
     print("Tus numeros son iguales")
-if numero<b:
-    print("El numero a es menor que b")
-if numero>b:
-    print("El numero a es mayor que b")
 
 input("...")
