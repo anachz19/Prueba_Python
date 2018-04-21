ede7673471ce63c42de4ae0b575681bedb844045
Ana Chávez <38160675+anachz19@users.noreply.github.com>
2018-04-21 09:11:44 -0500

    Area cuadrado y triangulo
    21/04/18

---
+++ area.py
@@ -0,0 +1,9 @@
+print("ingrese sus medidas")
+ladoA= int(input("Lado 1"))
+ladoB= int(input("Lado 2"))
+Resultado= ladoA*ladoA
+print("El area del cuadrado es", Resultado)
+Resultado2= (ladoA*ladoB)/2
+print("El area del triangulo es ", Resultado2)
+
+input("....")
