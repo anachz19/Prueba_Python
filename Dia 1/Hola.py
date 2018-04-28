b4bfd08ea8c83a3301c4072e28fb6240de0db8e1
Ana Chávez <38160675+anachz19@users.noreply.github.com>
2018-04-28 12:53:50 -0500

    Add files via upload

---
+++ Dia 1/Hola.py
@@ -0,0 +1,28 @@
+"""
+print("Hola, ¿Como te llamas?")
+Nombre= "Ana"
+Apellido= "Chavez"
+Edad=24
+Genero="Femenino"
+Comidafavorita= "Pizza"
+Signozodiacal="Escorpion"
+Colordeojos= "cafe"
+print(Nombre,"\n", Apellido,"\n", Edad,"\n", Genero,"\n", Comidafavorita,"\n", Signozodiacal,"\n", Colordeojos,"\n")   
+"""
+#Codigo2
+Nombre= input("Dame tu nombre: ")
+Apellido= input("Dame tu apellido: ")
+Edad= input("Dime tu edad: ")
+Genero= input("Genero: ")
+Comidafavorita= input("¿Cual es tu comida favorita? ")
+Signozodiacal= input("Sgino zodiacal: ")
+Colordeojos= input("Color de ojos: ")
+ 
+print("Nombre: ", Nombre,"\n",
+      "Apellido: ", Apellido,"\n",
+      "Edad: ",Edad,"\n",
+      "Genero: ", Genero,"\n",
+      "Comidafavorita: ", Comidafavorita,"\n",
+      "Signozodiacal: ", Signozodiacal,"\n",
+      "Colordeojos: ", Colordeojos,"\n")
+
