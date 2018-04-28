602956681e673f0c3f4f6c8fa07bc142f47c1a50
Ana Chávez <38160675+anachz19@users.noreply.github.com>
2018-04-28 12:55:39 -0500

    Delete IVA.py

--- IVA.py
+++
@@ -1,9 +0,0 @@
-print("Este programa calcula el IVA")
-Cantidad= float(input("Dame una cantidad"))
-IVA= float( input("Dame el IVA"))
-Resultado= Cantidad*IVA;
-print("Tu cantidad es: ", Cantidad, "y tu IVA es ", IVA,
-        "Tu cantidad total es: ", Resultado)
-
-input(".....")
-
